abf8e9b9fa161a3552fb5d96319aeff0db88b984
Jonathan Samson <jonathansamsonvcu@gmail.com>
2021-03-13 14:13:52 -0500

    add travis ci integration

--- src/editor.rs
+++ src/editor.rs
@@ -14,14 +14,16 @@ impl<'a> Editor<'a> {
 
     pub fn open_file_in_repo(&self, path: &PathBuf) -> Result<(), NoteError>{
         let mut path = path.clone();
-
         let filename = path.clone();
+
+        // create any directories, if incoming path is nested
         path.pop();
         let directory_path = path;
-
         if directory_path.as_os_str().len() != 0 {
             self.shell.execute_in_repo(&*format!("mkdir -p {:?}", directory_path))?;
         }
+
+        // open file
         self.shell.execute_in_repo(
             &format!("{} {:?}", self.command, filename)
         )?;
